4a132964c3da315dac0222b633264ad208e32d44
aldoromano <tiano.roma@gmail.com>
2022-12-08 09:53:43 +0100

    Intermediate update

--- routes/favorite.js
+++ routes/favorite.js
@@ -6,6 +6,9 @@ const router = express.Router();
 const Favorite = require("../models/Favorite");
 const User = require("../models/User");
 
+// Fichier des restaurants
+const restaurants = require("../assets/restaurants.json");
+
 router.post("/user/isfavorite", async (req, res) => {
   console.log("isfavorite ->> ", req.body);
   const { placeId, token } = req.body;
@@ -73,7 +76,7 @@ router.post("/user/favorite", async (req, res) => {
 
     if (favorite) {
       console.log("suppression ->> ", favorite._id);
-      await favorite.findByIdAndDelete(favorite._id);
+      await Favorite.findByIdAndDelete(favorite._id);
     } else {
       const newFavorite = new Favorite({
         placeId: placeId,
@@ -89,4 +92,56 @@ router.post("/user/favorite", async (req, res) => {
   }
 });
 
+router.post("/user/favorites", async (req, res) => {
+  console.log("user/favorites ... -> ", req.body);
+  const { token } = req.body;
+  try {
+    // Contrôle du token
+    // 1 - Obligatoire
+    if (!token) {
+      return res.status(400).json({ message: "Token mandatory" });
+    }
+
+    // Vérification du token
+    // 2 - Doit  exister
+    const user = await User.findOne({ token: token });
+    if (!user) {
+      return res.status(400).json({ message: "Credentials are incorrect" });
+    }
+
+    const favorites = await Favorite.find(
+      {
+        userId: user._id,
+      },
+      { placeId: 1, _id: 0 }
+    );
+
+    console.log("Nombre de favoris ->> ", favorites);
+
+    const favoriteRestaurants = restaurants.filter((elem, index) => {
+      //return favorites.includes(elem.placeId);
+
+      let matchCount = 0;
+      for (let i = 0; i < favorites.length; i++) {
+        // console.log(
+        //   "Comparaison : ",
+        //   favorites[i].placeId.toString(),
+        //   " - ",
+        //   elem.placeId.toString()
+        // );
+
+        if (favorites[i].placeId.toString() === elem.placeId.toString()) {
+          matchCount++;
+        }
+      }
+      if (matchCount) return true;
+      else return false;
+    });
+
+    console.log("Nombre de restaurants trouvés", favoriteRestaurants.length);
+    res.status(200).json(favoriteRestaurants);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = router;
